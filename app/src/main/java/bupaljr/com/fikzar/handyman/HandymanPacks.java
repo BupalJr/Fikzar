735ea5b2f76d5aa837e626362500b973ddbb1e1c
Abdirahman Ali <36969764+bupaljr@users.noreply.github.com>
2021-03-03 15:09:45 +0000

    Weekly Commits 0.7 - Handyman payment UI done

--- app/src/main/java/bupaljr/com/fikzar/handyman/HandymanPacks.java
+++ app/src/main/java/bupaljr/com/fikzar/handyman/HandymanPacks.java
@@ -6,17 +6,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import bupaljr.com.fikzar.R;
 
-public class HandymanPacks extends AppCompatActivity {
+public class HandymanPacks extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buy50, buy100, buy200, buyAny;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handyman_packs);
+
+        buy50 = findViewById(R.id.buy_50_credit_button);
+        buy50.setOnClickListener(this);
+        buy100 = findViewById(R.id.buy_100_credit_button);
+        buy100.setOnClickListener(this);
+        buy200 = findViewById(R.id.buy_200_credit_button);
+        buy200.setOnClickListener(this);
+        buyAny = findViewById(R.id.buy_credit_button);
+        buyAny.setOnClickListener(this);
 
 
         // Initialize variable
@@ -55,5 +69,23 @@ public class HandymanPacks extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buy_50_credit_button:
+                startActivity(new Intent(this, HandymanPaymentMethods.class));
+
+            case R.id.buy_credit_button:
+                startActivity(new Intent(this, HandymanPaymentMethods.class));
+
+            case R.id.buy_200_credit_button:
+                startActivity(new Intent(this, HandymanPaymentMethods.class));
+
+            case R.id.buy_100_credit_button:
+                startActivity(new Intent(this, HandymanPaymentMethods.class));
+
+        }
     }
 }
